4f4e33c72ff09d8eb2ada6716f9106d1d8a2f592
Nick Pavlovic <nickpavlovic@icloud.com>
2022-10-07 15:25:34 -0400

    added questions and license information

--- index.js
+++ index.js
@@ -22,6 +22,115 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const genMd = require('./utils/generateMarkdown.js');
+const licenseArrayName = [];
+const licenseArrayKey = [];
+const licenseListGitHub = [
+  {
+    "key": "agpl-3.0",
+    "name": "GNU Affero General Public License v3.0",
+    "spdx_id": "AGPL-3.0",
+    "url": "https://api.github.com/licenses/agpl-3.0",
+    "node_id": "MDc6TGljZW5zZTE="
+  },
+  {
+    "key": "apache-2.0",
+    "name": "Apache License 2.0",
+    "spdx_id": "Apache-2.0",
+    "url": "https://api.github.com/licenses/apache-2.0",
+    "node_id": "MDc6TGljZW5zZTI="
+  },
+  {
+    "key": "bsd-2-clause",
+    "name": "BSD 2-Clause \"Simplified\" License",
+    "spdx_id": "BSD-2-Clause",
+    "url": "https://api.github.com/licenses/bsd-2-clause",
+    "node_id": "MDc6TGljZW5zZTQ="
+  },
+  {
+    "key": "bsd-3-clause",
+    "name": "BSD 3-Clause \"New\" or \"Revised\" License",
+    "spdx_id": "BSD-3-Clause",
+    "url": "https://api.github.com/licenses/bsd-3-clause",
+    "node_id": "MDc6TGljZW5zZTU="
+  },
+  {
+    "key": "bsl-1.0",
+    "name": "Boost Software License 1.0",
+    "spdx_id": "BSL-1.0",
+    "url": "https://api.github.com/licenses/bsl-1.0",
+    "node_id": "MDc6TGljZW5zZTI4"
+  },
+  {
+    "key": "cc0-1.0",
+    "name": "Creative Commons Zero v1.0 Universal",
+    "spdx_id": "CC0-1.0",
+    "url": "https://api.github.com/licenses/cc0-1.0",
+    "node_id": "MDc6TGljZW5zZTY="
+  },
+  {
+    "key": "epl-2.0",
+    "name": "Eclipse Public License 2.0",
+    "spdx_id": "EPL-2.0",
+    "url": "https://api.github.com/licenses/epl-2.0",
+    "node_id": "MDc6TGljZW5zZTMy"
+  },
+  {
+    "key": "gpl-2.0",
+    "name": "GNU General Public License v2.0",
+    "spdx_id": "GPL-2.0",
+    "url": "https://api.github.com/licenses/gpl-2.0",
+    "node_id": "MDc6TGljZW5zZTg="
+  },
+  {
+    "key": "gpl-3.0",
+    "name": "GNU General Public License v3.0",
+    "spdx_id": "GPL-3.0",
+    "url": "https://api.github.com/licenses/gpl-3.0",
+    "node_id": "MDc6TGljZW5zZTk="
+  },
+  {
+    "key": "lgpl-2.1",
+    "name": "GNU Lesser General Public License v2.1",
+    "spdx_id": "LGPL-2.1",
+    "url": "https://api.github.com/licenses/lgpl-2.1",
+    "node_id": "MDc6TGljZW5zZTEx"
+  },
+  {
+    "key": "mit",
+    "name": "MIT License",
+    "spdx_id": "MIT",
+    "url": "https://api.github.com/licenses/mit",
+    "node_id": "MDc6TGljZW5zZTEz"
+  },
+  {
+    "key": "mpl-2.0",
+    "name": "Mozilla Public License 2.0",
+    "spdx_id": "MPL-2.0",
+    "url": "https://api.github.com/licenses/mpl-2.0",
+    "node_id": "MDc6TGljZW5zZTE0"
+  },
+  {
+    "key": "unlicense",
+    "name": "The Unlicense",
+    "spdx_id": "Unlicense",
+    "url": "https://api.github.com/licenses/unlicense",
+    "node_id": "MDc6TGljZW5zZTE1"
+  }
+];
+
+
+
+function extractLicenses() {
+  let i=0;
+  licenseArrayName.push("");
+  for(const lic of licenseListGitHub) {
+      i++;
+      licenseArrayName.push(`${i}. ${lic.name}`);
+      licenseArrayKey.push(lic.key);
+  }
+
+}
+
 
 // TODO: Create an array of questions for user input
 const questions = [      
@@ -31,28 +140,50 @@ const questions = [
     name: 'title',
   },
   {
-    type: 'input',
-    message: 'Where are you located?',
-    name: 'location',
+    type: 'editor',
+    name: 'description',
+    message: 'Enter the Description section content (press ESC and SHIFT-Z twice to exit the Editor):',
+    waitUserInput: true,
   },
   {
     type: 'editor',
-    message: 'Write a brief bio:',
-    name: 'bio',
-    
-  }, 
-  {
-      type: 'input',
-      message: 'What is your LinkedIn URL?',
-      name: 'LinkedIn',
-      
+    name: 'installation',
+    message: 'Enter the Installation section content (press ESC and SHIFT-Z twice to exit the Editor):',
+    waitUserInput: true,
   },
   {
-      type: 'input',
-      message: 'What is your GitHub URL?',
-      name: 'GitHub',
-      
-  }];
+    type: 'editor',
+    name: 'usage',
+    message: 'Enter the Usage section content (press ESC and SHIFT-Z twice to exit the Editor):',
+    waitUserInput: true,
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license to apply to License section:',
+    choices: licenseArrayName,
+  },
+  {
+    type: 'editor',
+    name: 'contributing',
+    message: 'Enter the Contributing section content (press ESC and SHIFT-Z twice to exit the Editor):',
+    waitUserInput: true,
+  },
+  {
+    type: 'editor',
+    name: 'tests',
+    message: 'Enter the Tests section content (press ESC and SHIFT-Z twice to exit the Editor):',
+    waitUserInput: true,
+  },
+  {
+  type: 'editor',
+  name: 'questions',
+  message: 'Enter the Questions section content (press ESC and SHIFT-Z twice to exit the Editor):',
+  waitUserInput: true,
+}
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -63,17 +194,43 @@ function writeToFile(fileName, data) {
               : console.log('README.MD file created!'));
 }
 
+const header =
+`=====================================================================================================
+=       ===        =====  =====       ===  =====  ==        ============       ===       =====    ===
+=  ====  ==  ==========    ====  ====  ==   ===   ==  ==================  ====  ==  ====  ===  ==  ==
+=  ====  ==  =========  ==  ===  ====  ==  =   =  ==  ==================  ====  ==  ====  ==  ====  =
+=  ===   ==  ========  ====  ==  ====  ==  == ==  ==  ==================  ====  ==  ===   ==  ====  =
+=      ====      ====  ====  ==  ====  ==  =====  ==      ====        ==       ===      ====  ====  =
+=  ====  ==  ========        ==  ====  ==  =====  ==  ==================  ========  ====  ==  ====  =
+=  ====  ==  ========  ====  ==  ====  ==  =====  ==  ==================  ========  ====  ==  ====  =
+=  ====  ==  ========  ====  ==  ====  ==  =====  ==  ==================  ========  ====  ===  ==  ==
+=  ====  ==        ==  ====  ==       ===  =====  ==        ============  ========  ====  ====    ===
+=====================================================================================================
+                                                                                                     
+┌┐ ┬ ┬  ╔╗╔┬┌─┐┬┌─  ╔═╗┌─┐┬  ┬┬  ┌─┐┬  ┬┬┌─┐
+├┴┐└┬┘  ║║║││  ├┴┐  ╠═╝├─┤└┐┌┘│  │ │└┐┌┘││  
+└─┘ ┴   ╝╚╝┴└─┘┴ ┴  ╩  ┴ ┴ └┘ ┴─┘└─┘ └┘ ┴└─┘`;
+
+
+
 // TODO: Create a function to initialize app
 function init() {
 
-    inquirer
-    .prompt(questions)
-    .then((response) => {
-  
-          writeToFile('README.MD',genMd.generateMarkdown(response));
+  extractLicenses();
+  console.log("\n");  
+  console.log(header);
+  console.log("\n");
 
-});
+  inquirer
+  .prompt(questions)
+  .then((response) => {
+    console.log(response);  
+    writeToFile('README.MD',genMd.generateMarkdown(response));
+
+  });
+
 }
+
 
 // Function call to initialize app
 init();
